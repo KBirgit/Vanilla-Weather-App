6eb59196edac594d2c5ae8225f6b7b46eae03eb0
Birgit <birgitkulli@gmail.com>
2020-11-16 12:49:44 +0200

    Added code for actual day of the week

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let timeDay = document.querySelector("#timeDay")
+let now = new Date();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[now.getDay()];
+
+timeDay.innerHTML = `${day}`;
