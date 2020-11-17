6dca14043fa25257b0caa32063491bc66dd79ec3
Birgit <birgitkulli@gmail.com>
2020-11-17 13:42:32 +0200

    Updated date to change every time when information in app is updated

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let timeDay = document.querySelector("#timeDay")
-let now = new Date();
+function formatDate(timestamp) {
+let date = new Date();
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
+let day = days[date.getDay()];
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-timeDay.innerHTML = `${day} ${hours}:${minutes}`;
+return `${day} ${hours}:${minutes}`;
+}
 
 function getLocation(response) {
   cityName = response.data.name;
@@ -28,6 +32,7 @@ function showWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#timeDay")
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
@@ -40,6 +45,7 @@ function showWeather(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(3.6 * response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt*1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
